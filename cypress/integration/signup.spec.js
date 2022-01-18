d40bb9b95b170d8062db5a16f78d0c6a5485b969
unknown <gabriellamreis@hotmail.com>
2022-01-18 20:22:25 -0300

    Create e2e tests with cypress

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,19 @@
+it('successfully signs up using confirmation code sent via email', () => {
+
+  const faker = require('faker')
+  const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+  const password = Cypress.env('USER_PASSWORD')
+
+  cy.intercept('GET', '**/notes').as('getNotes') // requisição para pegar as anotações já existentes
+  cy.fillSignupFormAndSubmit(emailAddress, password)
+
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: emailAddress
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0] // pega os 6 primeiros dígitos do email
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+
+    cy.wait('@getNotes') // espera a requisição terminar
+    cy.contains('h1', 'Your Notes').should('be.visible')
+  })
+})
---
+++ cypress/support/index.js
@@ -0,0 +1,6 @@
+import 'cypress-file-upload'
+import 'cypress-iframe'
+import 'cypress-mailosaur'
+import './commands'
+
+
