3bc008baf853520af687f584c25b02e7c44909ec
unknown <gabriellamreis@hotmail.com>
2022-01-18 21:07:00 -0300

    Configure cypress grep

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -31,7 +31,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
       expect(response.state).to.equal('Complete')
     })
   })
-  it('logs out', () => {
+  it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
     cy.wait('@getNotes')
 
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,5 +2,6 @@ import 'cypress-file-upload'
 import 'cypress-iframe'
 import 'cypress-mailosaur'
 import './commands'
+require('cypress-grep')()
 
 
